0e014b9cb0c66ac7f58b3233629e67cd1c43d6b0
Griffin Annis <annisg@rose-hulman.edu>
2021-01-20 17:49:41 -0500

    API stub

---
+++ src/API.java
@@ -0,0 +1,17 @@
+
+public class API implements PortObserver {
+
+    PortWatcher portWatcher;
+    
+    public API() {
+        
+        portWatcher = new PortWatcher(800, this);
+        portWatcher.listen();
+        
+    }
+    
+    public void recieveMessage(String message) {
+        
+    }
+    
+}
