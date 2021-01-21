ed2c0c5c6ed080a3c56eb17e73ee6a7a268460f5
moyersjm <moyersjm@rose-hulman.edu>
2021-01-20 20:28:58 -0500

    portwatcher listens for commandline input

--- src/PortWatcher.java
+++ src/PortWatcher.java
@@ -5,19 +5,31 @@ public class PortWatcher {
     private int port;
     private Scanner scanner;
     private PortObserver observer;
+    private int currentOrder;
     
     public PortWatcher(int port, PortObserver observer) {
         this.port = port;
         this.observer = observer;
         scanner = new Scanner(System.in);
+        currentOrder = 0;
     }
     
     public void listen() {
         
-        String input = scanner.next();
-        while(!input.equals("`")) {
-            input = scanner.nextLine();
-        }
+		System.out.println(String.format("Processing order %d", currentOrder));
+    	System.out.println("Please enter a drink name:");
+    	String drink = scanner.nextLine();
+    	System.out.println("Please enter an address:");
+    	String address = scanner.nextLine();
+    	System.out.println("Please enter the zipcode:");
+    	int zip = scanner.nextInt();
+    	
+    	Order o = new Order(currentOrder++, address, zip, drink);
+    	
+    	observer.recieveOrder(o);
+    	System.out.println();
+    	
+    	
     }
     
 }
