b90ccc9c8483c856adc4e52e8a5af3997c2695aa
moyersjm <moyersjm@rose-hulman.edu>
2021-01-27 19:06:20 -0500

    JSON file read and converted to an object

--- src/PortWatcher.java
+++ src/PortWatcher.java
@@ -1,3 +1,5 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Scanner;
 
 import com.github.cliftonlabs.json_simple.JsonObject;
@@ -24,26 +26,37 @@ public class PortWatcher {
     private boolean processInput() {
     	
     	JsonObject input = new JsonObject();
+    	try {
+			FileReader fr = new FileReader("src/jsons/order1.json");
+			input = (JsonObject) Jsoner.deserialize(fr);
+		} catch (Exception e) {
+			System.out.println("Ya done goofed");
+			e.printStackTrace();
+			return false;
+		}
     	
+    	System.out.println(input.toString());
+    	scanner.nextLine();
+    	return true;
     	
         
-		System.out.println(String.format("Processing order %d", currentOrder));
-    	System.out.println("Please enter a drink name:");
-    	String drink = scanner.nextLine();
-    	System.out.println("Please enter an address:");
-    	String address = scanner.nextLine();
-    	System.out.println("Please enter the zipcode:");
-    	int zip = scanner.nextInt();
-    	scanner.nextLine();
-    	
-    	Order o = new Order(currentOrder++, address, zip, drink);
-    	
-    	observer.recieveOrder(o);
-    	System.out.println();
-    	    	
-        System.out.println("Continue? (y/n)");
-        String cont = scanner.nextLine();
-        return cont.equals("y");
+//		System.out.println(String.format("Processing order %d", currentOrder));
+//    	System.out.println("Please enter a drink name:");
+//    	String drink = scanner.nextLine();
+//    	System.out.println("Please enter an address:");
+//    	String address = scanner.nextLine();
+//    	System.out.println("Please enter the zipcode:");
+//    	int zip = scanner.nextInt();
+//    	scanner.nextLine();
+//    	
+//    	Order o = new Order(currentOrder++, address, zip, drink);
+//    	
+//    	observer.recieveOrder(o);
+//    	System.out.println();
+//    	    	
+//        System.out.println("Continue? (y/n)");
+//        String cont = scanner.nextLine();
+//        return cont.equals("y");
     	
     }
     
