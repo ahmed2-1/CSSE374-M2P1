7d6897e6d84fd58c2421bd2f14c422cd611377c0
Griffin Annis <annisg@rose-hulman.edu>
2021-01-28 15:49:36 -0500

    compare json objects instead of strings

--- src/test/APIOutput_Tests.java
+++ src/test/APIOutput_Tests.java
@@ -4,6 +4,9 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.io.*;
 import org.junit.jupiter.api.Test;
 
+import com.github.cliftonlabs.json_simple.JsonObject;
+import com.github.cliftonlabs.json_simple.Jsoner;
+
 import data.ControllerDatabase;
 import domain.API;
 import presentation.PortWatcher;
@@ -13,44 +16,29 @@ class APIOutput_Tests {
     private final String controllerCommandPath = "src/jsons/controller-command-output.json";
     private final String userResponsePath = "src/jsons/api-user-response-output.json";
     
-    public String readControllerCommandOutput() {
+    public JsonObject readControllerCommandOutput() {
         return readFile(controllerCommandPath);
     }
     
-    public String readUserResponseOutput() {
+    public JsonObject readUserResponseOutput() {
         return readFile(userResponsePath);
     }
 
-    private String readFile(String path) {
+    private JsonObject readFile(String path) {
         BufferedReader input = null;
         try {
             input = new BufferedReader(new FileReader(path));
-        } catch (FileNotFoundException e) {
+            return  (JsonObject) Jsoner.deserialize(input);
+        } catch (Exception e) {
             e.printStackTrace();
-            fail("Failed to open file " + path);
-            return "";
+            fail("Failed to process file " + path);
+            return null;
         }
         
-        String result = "";
-        String in;
-        try {
-            in = input.readLine();
-            
-            while(in != null) {
-                result += in;
-                in = input.readLine();
-            }
-            
-            input.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-            fail("Failed to read file " + path);
-        }
-
-        return result;
     }
     
 
+    //Controller response is located at src/jsons/controller-response%d, where %d is the orderid of the input order
     void testWithInputs(String orderInPath, String expectedControllerCommandPath, String expectedUserResponsePath, String databasePath) {
         
         ControllerDatabase database = new ControllerDatabase(databasePath);
@@ -59,9 +47,12 @@ class APIOutput_Tests {
         
         watcher.processInput(orderInPath);
         
-        String orderIn = readFile(orderInPath);
-        String expectedControllerCommand = readFile(expectedControllerCommandPath);
-        String expectedUserResponse = readFile(expectedUserResponsePath);
+        JsonObject expectedControllerCommand = readFile(expectedControllerCommandPath);
+        JsonObject expectedUserResponse = readFile(expectedUserResponsePath);
+        
+        System.out.println(expectedControllerCommand);
+        System.out.println(readControllerCommandOutput());
+        
         
         assertEquals(expectedControllerCommand, readControllerCommandOutput(), "Expected command sent to controller differs from actual");
         assertEquals(expectedUserResponse, readUserResponseOutput(), "Expected response sent to user differs from actual");
