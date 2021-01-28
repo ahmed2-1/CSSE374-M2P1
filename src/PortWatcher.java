9a74430e64b4220ecbc8842444aae51c54ff85c5
moyersjm <moyersjm@rose-hulman.edu>
2021-01-27 20:50:44 -0500

    Fixed bug in Portwatcher, things work better now

--- src/PortWatcher.java
+++ src/PortWatcher.java
@@ -63,11 +63,13 @@ public class PortWatcher {
     	JsonArray condiments = (JsonArray) details.getCollection(OrderKey.CONDIMENTS);
     	ArrayList<Option> options = new ArrayList<>();
     	
-    	for (Object o : condiments) {
-    		JsonObject cond = (JsonObject) o;
-    		String name = cond.getString(OrderKey.NAME);
-    		int qty = cond.getInteger(OrderKey.QUANTITY);
-    		options.add(new Option(name, qty));
+    	if (condiments != null) {
+	    	for (Object o : condiments) {
+	    		JsonObject cond = (JsonObject) o;
+	    		String name = cond.getString(OrderKey.NAME);
+	    		int qty = cond.getInteger(OrderKey.QUANTITY);
+	    		options.add(new Option(name, qty));
+	    	}
     	}
     	
     	
